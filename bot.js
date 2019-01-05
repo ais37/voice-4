7bbfcaa12ef3ca6d12a321016619bdb2d5ccb8ff
shadowssxx <43233768+shadowssxx@users.noreply.github.com>
2019-01-05 11:35:51 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+﻿var Discord = require('discord.js');
+var client = new Discord.Client();
+
+client.on('message', message => {
+  if (!message.guild) return;
+
+  if (message.content === 'shadow1') {
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { 
+          message.reply('لقد دخلت الروم بنجاح !');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('يجب ان تكون في روم صوتي');
+    }
+  }
+});
+  client.login(process.env.BOT_TOKEN);
